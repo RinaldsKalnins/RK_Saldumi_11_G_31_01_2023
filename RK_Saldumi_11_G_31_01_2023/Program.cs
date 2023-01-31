8ca0f2f9c995ffd9d6a104c607b19f032692b1a5
RinaldsKalnins <rinaldskalnins972@gmail.com>
2023-01-31 08:45:47 +0200

    Init Commit

---
+++ RK_Saldumi_11_G_31_01_2023/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RK_Saldumi_11_G_31_01_2023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
